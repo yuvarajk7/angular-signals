7374c9f0ad4eeb123fcf3959e2557d71ce01e758
yuvarajk7 <yuvaraj.kesavan@hotmail.com>
2025-02-20 12:09:26 -0600

    created angular signal project

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import {Component, computed, effect, inject, Injector, signal} from '@angular/core';
+import {CoursesService} from "../services/courses.service";
+import {Course, sortCoursesBySeqNo} from "../models/course.model";
+import {MatTab, MatTabGroup} from "@angular/material/tabs";
+import {CoursesCardListComponent} from "../courses-card-list/courses-card-list.component";
+import {MatDialog} from "@angular/material/dialog";
+import {MessagesService} from "../messages/messages.service";
+import {catchError, from, throwError} from "rxjs";
+import {toObservable, toSignal, outputToObservable, outputFromObservable} from "@angular/core/rxjs-interop";
+
+@Component({
+    selector: 'home',
+    imports: [
+        MatTabGroup,
+        MatTab,
+        CoursesCardListComponent
+    ],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
